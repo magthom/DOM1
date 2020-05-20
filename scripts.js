086457841003675a8a816140b67aa3538585865f
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-20 15:46:58 -0500

    appended elements to body

--- scripts.js
+++ scripts.js
@@ -6,5 +6,10 @@ document.addEventListener("DOMContentLoaded", function () {
     h1.appendChild(h1Text);
     div.appendChild(h1);
     document.body.appendChild(div);
+    let h2 = document.createElement('h2');
+    let h2Text = document.createTextNode('This is an h2.')
+    h2.appendChild(h2Text);
+    div.appendChild(h2);
+    document.body.appendChild(div);
 
 })

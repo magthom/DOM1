91f0090621a0020360041d0c48c103d823e5ecf7
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-20 15:54:46 -0500

    repeated steps for h3 through h6

--- scripts.js
+++ scripts.js
@@ -11,5 +11,25 @@ document.addEventListener("DOMContentLoaded", function () {
     h2.appendChild(h2Text);
     div.appendChild(h2);
     document.body.appendChild(div);
+    let h3 = document.createElement('h3');
+    let h3Text = document.createTextNode('This is an h3.');
+    h3.appendChild(h3Text);
+    div.appendChild(h3);
+    document.body.appendChild(div);
+    let h4 = document.createElement('h4');
+    let h4Text = document.createTextNode('This is an h4.');
+    h4.appendChild(h4Text);
+    div.appendChild(h4);
+    document.body.appendChild(div);
+    let h5 = document.createElement('h5');
+    let h5Text = document.createTextNode('This is an h5.');
+    h5.appendChild(h5Text);
+    div.appendChild(h5);
+    document.body.appendChild(div);
+    let h6 = document.createElement('h6');
+    let h6Text = document.createTextNode('This is an h6.');
+    h6.appendChild(h6Text);
+    div.appendChild(h6);
+    document.body.appendChild(div);
 
 })
